0befd4c789b4460c96308f1f45b6737195ccfce9
princeemeka965 <anyanwue4@gmail.com>
2023-06-20 18:05:21 +0100

    formatting address

--- locationAPI/setLocator.js
+++ locationAPI/setLocator.js
@@ -1,7 +1,7 @@
 import { router as _router, axios, conn } from "../header/appHeader.js";
 
 _router.get("/", function (req, res, next) {
-    const address = "7 Harmony Road Ugwuorie Iji-Nike Enugu"
+    const address = "Independence%20Layout%20Enugu"
     axios.get(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_KEY}`).then((response) => {
         res.status(200).send({
             response
