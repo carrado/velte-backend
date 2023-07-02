c2c1ad2d258ff42b1660c1f34a047db0cd5ed6bd
princeemeka965 <anyanwue4@gmail.com>
2023-07-02 21:18:19 +0100

    set error message

--- locationAPI/setLocator.js
+++ locationAPI/setLocator.js
@@ -23,7 +23,9 @@ _router.get("/nearby_search", function (req, res, next) {
             });
         })
         .catch((e) => {
-            console.log(e);
+            res.status(500).send({
+                err: "No result found"
+            })
         });
 });
 
