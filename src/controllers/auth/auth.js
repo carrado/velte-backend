859e28ee7ebefe1aca330de761686828f87fad16
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2026-01-11 05:42:36 +0100

    Update cookie domain for production environment in login controller

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -174,7 +174,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      domain: process.env.NODE_ENV === "production" ? ".velte.ng" : "localhost",
+      domain: process.env.NODE_ENV === "production" ? "velte-frontend.netlify.app" : "localhost",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
