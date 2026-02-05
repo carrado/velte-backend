52d187168e772a7214ef0a7c7cd6d244dd3ef66e
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2026-02-05 12:36:32 +0100

    Update cookie settings to support staging environment in auth controller

--- src/controllers/auth/auth.js
+++ src/controllers/auth/auth.js
@@ -98,8 +98,8 @@ export const register = async (req, res) => {
     // 🔹 Set HttpOnly cookie
     res.cookie("auth_token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production" || process.env.NODE_ENV === 'staging',
+      sameSite: process.env.NODE_ENV === "production" || process.env.NODE_ENV === 'staging' ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -170,8 +170,8 @@ export const login = async (req, res) => {
     // 🔹 Set token in HttpOnly cookie
     res.cookie("auth_token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: process.env.NODE_ENV === "production" || process.env.NODE_ENV === 'staging',
+      sameSite: process.env.NODE_ENV === "production" || process.env.NODE_ENV === 'staging' ? "none" : "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
